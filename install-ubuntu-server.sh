b3865c9c340545f80647a1377a00d3839ada0b71
EC2 Default User <ec2-user@ip-10-61-42-169.ec2.internal>
2017-12-21 16:45:24 +0000

    add initial ubuntu support

--- install-ubuntu-server.sh
+++ install-ubuntu-server.sh
@@ -6,7 +6,6 @@
 ###########################
 
 # include my library helpers for colorized echo and require_brew, etc
-source ./lib_sh/utils.sh
 source ./lib_sh/echos.sh
 source ./lib_sh/requirers.sh
 
@@ -143,51 +142,16 @@ fi
 # install homebrew (CLI Packages)
 #####
 
-running "checking homebrew install"
-brew_bin=$(which brew) 2>&1 > /dev/null
-if [[ $? != 0 ]]; then
-  action "installing homebrew"
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    if [[ $? != 0 ]]; then
-      error "unable to install homebrew, script $0 abort!"
-      exit 2
-  fi
-else
-  ok
-  # Make sure we’re using the latest Homebrew
-  running "updating homebrew"
-  brew update
-  ok
-  bot "before installing brew packages, we can upgrade any outdated packages."
-  read -r -p "run brew upgrade? [y|N] " response
-  if [[ $response =~ ^(y|yes|Y) ]];then
-      # Upgrade any already-installed formulae
-      action "upgrade brew packages..."
-      brew upgrade
-      ok "brews updated..."
-  else
-      ok "skipped brew package upgrades.";
-  fi
-fi
-
-#####
-# install brew cask (UI Packages)
-#####
-running "checking brew-cask install"
-output=$(brew tap | grep cask)
-if [[ $? != 0 ]]; then
-  action "installing brew-cask"
-  require_brew caskroom/cask/brew-cask
-fi
-brew tap caskroom/versions > /dev/null 2>&1
-ok
+running "Updating Packages"
+update
+upgrade
 
 # skip those GUI clients, git command-line all the way
-require_brew git
+install_package "Git" "git"
 # need fontconfig to install/build fonts
-require_brew fontconfig
+install_package "fontconfig" "fontconfig"
 # update zsh to latest
-require_brew zsh
+install_package "ZSH" "zsh"
 # update ruby to latest
 # use versions of packages installed with homebrew
 RUBY_CONFIGURE_OPTS="--with-openssl-dir=`brew --prefix openssl` --with-readline-dir=`brew --prefix readline` --with-libyaml-dir=`brew --prefix libyaml`"
@@ -238,16 +202,16 @@ vim +PluginInstall +qall > /dev/null 2>&1
 
 bot "installing fonts"
 ./fonts/install.sh
-brew tap caskroom/fonts
-require_cask font-fontawesome
-require_cask font-awesome-terminal-fonts
-require_cask font-hack
-require_cask font-inconsolata-dz-for-powerline
-require_cask font-inconsolata-g-for-powerline
-require_cask font-inconsolata-for-powerline
-require_cask font-roboto-mono
-require_cask font-roboto-mono-for-powerline
-require_cask font-source-code-pro
+# brew tap caskroom/fonts
+# require_cask font-fontawesome
+# require_cask font-awesome-terminal-fonts
+# require_cask font-hack
+# require_cask font-inconsolata-dz-for-powerline
+# require_cask font-inconsolata-g-for-powerline
+# require_cask font-inconsolata-for-powerline
+# require_cask font-roboto-mono
+# require_cask font-roboto-mono-for-powerline
+# require_cask font-source-code-pro
 ok
 
 if [[ -d "/Library/Ruby/Gems/2.0.0" ]]; then
@@ -257,7 +221,7 @@ if [[ -d "/Library/Ruby/Gems/2.0.0" ]]; then
 fi
 
 # node version manager
-require_brew nvm
+bash -c "$(wget -qO - https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh)"
 
 # nvm
 require_nvm stable
@@ -276,79 +240,8 @@ bot "installing npm tools needed to run this project..."
 npm install
 ok
 
-bot "installing packages from config.js..."
-node index.js
-ok
+# bot "installing packages from config.js..."
+# node index.js
+# ok
 
-running "cleanup homebrew"
-brew cleanup > /dev/null 2>&1
-ok
-
-###############################################################################
-bot "Configuring General System UI/UX..."
-###############################################################################
-# Close any open System Preferences panes, to prevent them from overriding
-# settings we’re about to change
-running "closing any system preferences to prevent issues with automated changes"
-osascript -e 'tell application "System Preferences" to quit'
-ok
-
-
-###############################################################################
-bot "Terminal & iTerm2"
-###############################################################################
-
-# running "Only use UTF-8 in Terminal.app"
-# defaults write com.apple.terminal StringEncodings -array 4;ok
-#
-# running "Use a modified version of the Solarized Dark theme by default in Terminal.app"
-TERM_PROFILE='Solarized Dark xterm-256color';
-CURRENT_PROFILE="$(defaults read com.apple.terminal 'Default Window Settings')";
-if [ "${CURRENT_PROFILE}" != "${TERM_PROFILE}" ]; then
-	open "./configs/${TERM_PROFILE}.terminal";
-	sleep 1; # Wait a bit to make sure the theme is loaded
-	defaults write com.apple.terminal 'Default Window Settings' -string "${TERM_PROFILE}";
-	defaults write com.apple.terminal 'Startup Window Settings' -string "${TERM_PROFILE}";
-fi;
-
-#running "Enable “focus follows mouse” for Terminal.app and all X11 apps"
-# i.e. hover over a window and start `typing in it without clicking first
-defaults write com.apple.terminal FocusFollowsMouse -bool true
-#defaults write org.x.X11 wm_ffm -bool true;ok
-running "Installing the Solarized Light theme for iTerm (opening file)"
-open "./configs/Solarized Light.itermcolors";ok
-running "Installing the Patched Solarized Dark theme for iTerm (opening file)"
-open "./configs/Solarized Dark Patch.itermcolors";ok
-
-running "Don’t display the annoying prompt when quitting iTerm"
-defaults write com.googlecode.iterm2 PromptOnQuit -bool false;ok
-running "hide tab title bars"
-defaults write com.googlecode.iterm2 HideTab -bool true;ok
-running "set system-wide hotkey to show/hide iterm with ^\`"
-defaults write com.googlecode.iterm2 Hotkey -bool true;ok
-running "hide pane titles in split panes"
-defaults write com.googlecode.iterm2 ShowPaneTitles -bool false;ok
-running "animate split-terminal dimming"
-defaults write com.googlecode.iterm2 AnimateDimming -bool true;ok
-defaults write com.googlecode.iterm2 HotkeyChar -int 96;
-defaults write com.googlecode.iterm2 HotkeyCode -int 50;
-defaults write com.googlecode.iterm2 FocusFollowsMouse -int 1;
-defaults write com.googlecode.iterm2 HotkeyModifiers -int 262401;
-running "Make iTerm2 load new tabs in the same directory"
-/usr/libexec/PlistBuddy -c "set \"New Bookmarks\":0:\"Custom Directory\" Recycle" ~/Library/Preferences/com.googlecode.iterm2.plist
-running "setting fonts"
-defaults write com.googlecode.iterm2 "Normal Font" -string "Hack-Regular 12";
-defaults write com.googlecode.iterm2 "Non Ascii Font" -string "RobotoMonoForPowerline-Regular 12";
-ok
-running "reading iterm settings"
-defaults read -app iTerm > /dev/null 2>&1;
-ok
-
-
-###############################################################################
-# Kill affected applications                                                  #
-###############################################################################
-bot "OK. Note that some of these changes require a logout/restart to take effect."
-
-
-bot "Woot! All done. Kill this terminal and launch iTerm"
+bot "Woot! All done. You may want to restart you terminal now."
--- lib_sh/requirers.sh
+++ lib_sh/requirers.sh
@@ -96,3 +96,43 @@ function require_nvm() {
     fi
     ok
 }
+
+install_package() {
+
+    declare -r PACKAGE="$2"
+    declare -r PACKAGE_READABLE_NAME="$1"
+
+    if ! package_is_installed "$PACKAGE"; then
+        execute "sudo apt-get install --allow-unauthenticated -qqy $PACKAGE" "$PACKAGE_READABLE_NAME"
+        #                                      suppress output ─┘│
+        #            assume "yes" as the answer to all prompts ──┘
+    else
+        print_success "$PACKAGE_READABLE_NAME"
+    fi
+
+}
+
+package_is_installed() {
+    dpkg -s "$1" &> /dev/null
+}
+
+update() {
+
+    # Resynchronize the package index files from their sources.
+
+    execute \
+        "sudo apt-get update -qqy" \
+        "APT (update)"
+
+}
+
+upgrade() {
+
+    # Install the newest versions of all packages installed.
+
+    execute \
+        "export DEBIAN_FRONTEND=\"noninteractive\" \
+            && sudo apt-get -o Dpkg::Options::=\"--force-confnew\" upgrade -qqy" \
+        "APT (upgrade)"
+
+}
