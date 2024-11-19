75fdba13d4e996a664a74b97606c0b005e6802da
Abdul Hagi <muridiv.1@gmail.com>
2024-11-19 11:04:31 -0500

    Update tfenv.sh
    improved adding line to .profile

--- ubuntu/tfenv.sh
+++ ubuntu/tfenv.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
 # Check if git is installed
-if ! command -v git &> /dev/null
-then
+if ! command -v git &> /dev/null; then
     echo "git is required but it's not installed. Install git and run the script again."
-    exit
+    exit 1
 fi
 
 # Define installation directory
 TFENV_ROOT="${HOME}/.tfenv"
 
+# Function to append to a new line in a file
+append_to_new_line() {
+    local file=$1
+    local line=$2
+
+    # Check if the file ends with a newline
+    if [ -s "$file" ] && [ -n "$(tail -c 1 "$file")" ]; then
+        echo "" >> "$file"
+    fi
+
+    echo "$line" >> "$file"
+}
+
 # Check if tfenv is already installed
 if [ -d "$TFENV_ROOT" ]; then
     if ! grep -q 'export PATH="$HOME/.tfenv/bin:$PATH"' ~/.profile; then
-        echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.profile
+        append_to_new_line ~/.profile 'export PATH="$HOME/.tfenv/bin:$PATH"'
     fi
     echo "tfenv is already installed"
 else
@@ -21,7 +33,7 @@ else
     git clone https://github.com/tfutils/tfenv.git ~/.tfenv
 
     # Add tfenv to path
-    echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.profile
+    append_to_new_line ~/.profile 'export PATH="$HOME/.tfenv/bin:$PATH"'
 
     # Reload profile
     source ~/.profile
@@ -29,4 +41,3 @@ else
     # Verify the installation
     tfenv --version
 fi
-
